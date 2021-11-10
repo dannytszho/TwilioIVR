7f6c21d071536a09815f80537cc850e289651d64
dannytszho <dannytszho@gmail.com>
2021-11-10 13:18:23 -0800

    added a unit test

--- src/ivr/router.js
+++ src/ivr/router.js
@@ -1,5 +1,5 @@
 const Router = require("express").Router;
-const { welcome, action } = require("./handler");
+const { welcome, action, leaveMessage } = require("./handler");
 
 const router = new Router();
 
@@ -12,6 +12,12 @@ router.post("/welcome", (req, res) => {
 router.post("/action", (req, res) => {
   const digit = req.body.Digits;
   return res.send(action(digit));
+});
+
+// POST: /ivr/message
+router.post("/message", (req, res) => {
+  const message = req.body;
+  return res.type("text/xml").send(leaveMessage(message));
 });
 
 module.exports = router;
